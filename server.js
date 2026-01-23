58912108d7ac7b591a60d6e4977695f5f1bffc29
ianvanh <publishort1522@gmail.com>
2026-01-22 20:08:10 -0500

    actualizacion v1

--- server.js
+++ server.js
@@ -29,6 +29,16 @@ app.get('/service-worker.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/js/pwa.js'));
 });
 
+app.get('/ping', (req, res) => {
+  res.send('Pong');
+});
+
+setInterval(() => {
+  fetch('https://delivery-cgam.onrender.com/ping')
+    .then(res => console.log('Ping interno enviado:', res.status))
+    .catch(err => console.error('Error en el ping:', err.message));
+}, 14 * 60 * 1000);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
